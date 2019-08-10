bc90b5f703b608b1f8ad6cbc729a5c55e81c9858
Mauricio R Duarte <mauriciotem@gmail.com>
2019-08-10 15:12:32 -0300

    all routes configured

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,37 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from './config/multer';
+import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
+import SessionController from './app/controllers/SessionController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 // routes intercepted by auth middleware
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/meetups', MeetupController.store);
+routes.get('/meetups', MeetupController.index);
+routes.put('/meetups/:id', MeetupController.update);
+routes.delete('/meetups/:id', MeetupController.destroy);
+
+routes.get('/subscriptions', SubscriptionController.index);
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
+
+routes.get('/organizing', OrganizingController.index);
+routes.get('/organizing/:id', OrganizingController.show);
 
 export default routes;
