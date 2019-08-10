6a763eb6d70a7f0031efda6a7886cbdce22eb685
Mauricio R Duarte <mauriciotem@gmail.com>
2019-08-10 13:49:19 -0300

    first commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello, World' }));
+routes.post('/users', UserController.store);
+
+// routes intercepted by auth middleware
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
