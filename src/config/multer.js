d13a7044b9dbf052f14b22142d09fce729a85f58
Mauricio R Duarte <mauriciotem@gmail.com>
2019-08-10 15:07:13 -0300

    multer for files config

---
+++ src/config/multer.js
@@ -0,0 +1,36 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  fileFilter(req, file, cb) {
+    const types = ['jpeg', 'jpg', 'png'];
+    const typesRegex = new RegExp(types.join('|'), 'i');
+
+    const mime = typesRegex.test(file.mimetype);
+    const ext = typesRegex.test(extname(file.originalname));
+
+    if (mime && ext) {
+      return cb(null, true);
+    }
+
+    return cb({
+      message: `File upload only supports the following types: ${types.join(
+        ', '
+      )}`,
+    });
+  },
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+  limits: {
+    fileSize: 2 * (1024 * 1024),
+  },
+};
